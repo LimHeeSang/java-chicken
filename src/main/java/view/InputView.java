57344cff1c588267b7c613645e4f7acfe0c3c934
limheesang <dlagmltkd12@naver.com>
2022-12-04 02:08:02 +0900

    feat: 원하는 기능 선택 입력 기능

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,10 +3,18 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
+
+    private static final String INPUT_FEATURE_NUMBER_MESSAGE = "## 원하는 기능을 선택하세요.";
+    private static final String INPUT_TABLE_NUMBER_MESSAGE = "## 주문할 테이블을 선택하세요.";
     private static final Scanner scanner = new Scanner(System.in);
 
+    public static int inputFeatureNumber() {
+        System.out.println(INPUT_FEATURE_NUMBER_MESSAGE);
+        return scanner.nextInt();
+    }
+
     public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
+        System.out.println(INPUT_TABLE_NUMBER_MESSAGE);
         return scanner.nextInt();
     }
 }
