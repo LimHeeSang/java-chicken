b81650b94b804669c210c87c2eb274afdfc34337
limheesang <dlagmltkd12@naver.com>
2022-12-04 02:11:26 +0900

    feat: 메뉴 선택 입력 기능

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
 
     private static final String INPUT_FEATURE_NUMBER_MESSAGE = "## 원하는 기능을 선택하세요.";
     private static final String INPUT_TABLE_NUMBER_MESSAGE = "## 주문할 테이블을 선택하세요.";
+    private static final String INPUT_MENU_NUMBER_MESSAGE = "## 등록할 메뉴를 선택하세요.";
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int inputFeatureNumber() {
@@ -15,6 +16,11 @@ public class InputView {
 
     public static int inputTableNumber() {
         System.out.println(INPUT_TABLE_NUMBER_MESSAGE);
+        return scanner.nextInt();
+    }
+
+    public static int inputMenuNumber() {
+        System.out.println(INPUT_MENU_NUMBER_MESSAGE);
         return scanner.nextInt();
     }
 }
