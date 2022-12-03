81802fff013d38310efda7d3a9484fb6eea8b007
limheesang <dlagmltkd12@naver.com>
2022-12-04 02:12:24 +0900

    feat: 메뉴 수량 선택 입력 기능

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
     private static final String INPUT_FEATURE_NUMBER_MESSAGE = "## 원하는 기능을 선택하세요.";
     private static final String INPUT_TABLE_NUMBER_MESSAGE = "## 주문할 테이블을 선택하세요.";
     private static final String INPUT_MENU_NUMBER_MESSAGE = "## 등록할 메뉴를 선택하세요.";
+    private static final String INPUT_MENU_COUNT_MESSAGE = "## 메뉴의 수량을 입력하세요.";
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int inputFeatureNumber() {
@@ -21,6 +22,11 @@ public class InputView {
 
     public static int inputMenuNumber() {
         System.out.println(INPUT_MENU_NUMBER_MESSAGE);
+        return scanner.nextInt();
+    }
+
+    public static int inputMenuCount() {
+        System.out.println(INPUT_MENU_COUNT_MESSAGE);
         return scanner.nextInt();
     }
 }
